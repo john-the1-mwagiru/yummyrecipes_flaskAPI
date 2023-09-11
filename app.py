43b23883b2f720ee88ec0e545da192169b4a6a22
john-the1-mwagiru <johnmwagiru199@gmail.com>
2023-09-11 09:07:11 +0300

    feat-3:intergrated Flask-SQLAlchemy and Flask-Migrate into my app.py

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from marshmallow import Schema, fields
+import datetime
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "asecret"
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "postgresql://postgres:Kenya2030**@localhost:5433/Yummy Recipes"
+app.app_context().push()
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 @app.route("/")
